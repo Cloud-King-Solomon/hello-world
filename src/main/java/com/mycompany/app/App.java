27728b74b24c7d28e16e43e20a7aa4d709db3a2e
solomonlane <lane197@purdue.edu>
2024-11-06 18:39:12 -0500

    changed again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,10 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Goodbye World!" );
+        System.out.println( " World!" );
     }
 
     public static String sayIt() {
-        return "Hello World!";
+        return "Boo World!";
     }
 }
