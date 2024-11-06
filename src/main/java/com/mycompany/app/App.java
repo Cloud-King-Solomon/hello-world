922f212e8ad4dfa05d2b0497397e3a59bfb694d3
SolomonJL72 <lane197@purdue.edu>
2024-11-06 18:43:03 -0500

    Sjl test (#5)
    * changed
    
    * changed again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,10 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Goodbye World!" );
+
+        System.out.println( " World!" );
+
     }
 
     public static String sayIt() {
-        return "Hello World!";
+        return "Boo World!";
     }
 }
